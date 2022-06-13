09c5a5e60ab665d1d9ca7e6c5028597f4dbb5002
Marlyson dos Santos Mendes <50031055+MarlysonMendes@users.noreply.github.com>
2022-06-13 15:28:20 -0300

    criando ViewComponent  ShoppingCartResume

---
+++ Lanchonete/Components/ShoppingCartResume.cs
@@ -0,0 +1,36 @@
+﻿using Lanchonete.Models;
+using Lanchonete.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanchonete.Components
+{
+    public class ShoppingCartResume : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartResume(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //var itens = _shoppingCart.GetItensCart();
+            var itens = new List<ShoppingCartItem>()
+            {
+                new ShoppingCartItem(),
+                new ShoppingCartItem()
+
+            };
+            _shoppingCart.ShoppingCartItems = itens;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                TotalCartShopping = _shoppingCart.GetTotalAmountShoppingCart()
+            };
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
