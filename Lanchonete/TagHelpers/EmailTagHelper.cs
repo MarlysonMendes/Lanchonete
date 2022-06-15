2a9cd964ae0143233ad91575a8bc1af60ac0c569
Marlyson dos Santos Mendes <50031055+MarlysonMendes@users.noreply.github.com>
2022-06-14 23:39:42 -0300

    Implementando EmailTagHelper

---
+++ Lanchonete/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Lanchonete.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string EmailAddress { get; set; }
+        public string Content { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href","meilto:"+ EmailAddress);
+            output.Content.SetContent(Content);
+        }
+    }
+}
